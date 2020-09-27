ae69d21fcd7441b1b32c5422823e5ab58ce7bae7
damilola <damilolaadetoro1@gmail.com>
2020-09-27 13:49:21 +0100

    error check

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 import markdown2
 from . import util
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
 		return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()})
 def rand(request,entry):
+
 	data=util.get_entry(entry)
 	return render(
 		request, "encyclopedia/results.html", {'data':util.get_entry(entry),
@@ -14,18 +16,22 @@ def rand(request,entry):
 		)
 
 def searchh(request):
-	data=request.GET.get('q', '')
-
-	if (util.get_entry(data)) is not None:
-		return HttpResponseRedirect(reverse("rand", kwargs={'entry':data}))
-	else:
-		lst=util.list_entries()
-		matches=[]
-		for i in lst:
-			if data==i:
-				matches.append(data)
-			elif data in i:
-				matches.append(data)
-		return render(request, "encyclopedia/index.html", {"entries": matches})
+	if request.method=='POST':
+		
+		data=request.POST.get('q')
+		if (util.get_entry(data)) is not None:
+			return HttpResponseRedirect(reverse("rand", kwargs={'entry':data}))
+		else:
+			lst=util.list_entries()
+			matches=[]
+			for i in lst:
+			#if data==i:
+				#matches.append(data)
+				if data.upper() in i.upper():
+					matches.append(i)
+					return render(request, "encyclopedia/index.html", {
+						"entries": matches,
+						'search':True,
+						'value':data})
 	
 
